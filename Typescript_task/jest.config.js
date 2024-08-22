8c3c1effd50c206df27abf61a34d62c4e7b85e36
Danil Lipatnikov <vineyardone1@gmail.com>
2024-08-22 21:17:32 +0300

    initial commit

---
+++ Typescript_task/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Применение babel-jest для всех JS/TS файлов
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
